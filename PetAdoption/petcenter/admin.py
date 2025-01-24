646ff9e0a865351483bc30a7b50f9c4f5825f0b3
Huxley Nyaoga <huxleynyaoga@proton.me>
2025-01-24 21:13:04 +0300

    Modified admin panel

--- PetAdoption/petcenter/admin.py
+++ PetAdoption/petcenter/admin.py
@@ -1,8 +1,31 @@
 from django.contrib import admin
 from .models import * 
 
-admin.site.register(Pet)
-admin.site.register(PetAppearnace)
-admin.site.register(PetHealth)
-admin.site.register(PetBehaviour)
-admin.site.register(PetAdoption)
+
+class PetAdmin(admin.ModelAdmin):
+    list_display = ["name", "age", "species", "breed", "gender", "size"]
+    list_filter = ["breed"]
+    search_fields = ["name"]
+    
+    
+class PetAppearnaceAdmin(admin.ModelAdmin):
+    list_display = ["pet", "color", "weight"]
+    
+    
+class PetHealthAdmin(admin.ModelAdmin):
+    list_display = ["pet", "vaccinated", "neutered", "dewormed", "special_needs"]
+    
+
+class PetBehaviourAdmin(admin.ModelAdmin):
+    list_display = ["pet", "temperament", "activity_level", "compatibility"]
+    
+
+class PetAdoptionAdmin(admin.ModelAdmin):
+    list_display = ["pet", "availability", "adoption_fee", "location"]
+    
+    
+admin.site.register(Pet, PetAdmin)
+admin.site.register(PetAppearnace, PetAppearnaceAdmin)
+admin.site.register(PetHealth, PetHealthAdmin)
+admin.site.register(PetBehaviour, PetBehaviourAdmin)
+admin.site.register(PetAdoption, PetAdoptionAdmin)
